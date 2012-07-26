7adc02b6dc5394bcc75f386930b007aec5434121
gkuhn1 <gkuhn1@gkuhn1-linux.(none)>
2012-07-25 22:46:23 -0300

    Added setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@
 
 from setuptools import setup, find_packages
 
-setup(name='django_admin_bootstrap',
+template_dirs = [
+    'templates/admin/auth/user/*.html',
+    'templates/admin/edit_inline/*.html',
+    'templates/admin/includes/*.html',
+    'templates/admin/*.html',
+    'templates/registration/*.html',
+]
+
+setup(name='admin_bootstrap',
       version='0.1',
       description='Provides a Twitter bootstrap based interface for django admin.',
       author='Guilherme Kuhn',
@@ -10,5 +18,6 @@ setup(name='django_admin_bootstrap',
       url='https://github.com/gkuhn1/django-admin-templates-twitter-bootstrap',
       packages=find_packages(),
       license='GPL',
+      package_data={ 'admin_bootstrap': template_dirs },
       include_package_data=True,
      )
