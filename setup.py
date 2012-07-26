66cc8b60d6bf787a068659697a746ae8b0849b15
gkuhn1 <gkuhn1@gkuhn1-linux.(none)>
2012-07-25 22:14:37 -0300

    Added setup.py and changed admin_bootstrap into an django app

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django_admin_bootstrap',
+      version='0.1',
+      description='Provides a Twitter bootstrap based interface for django admin.',
+      author='Guilherme Kuhn',
+      author_email='g.kuhn0@gmail.com',
+      url='https://github.com/gkuhn1/django-admin-templates-twitter-bootstrap',
+      packages=find_packages(),
+      license='GPL',
+      include_package_data=True,
+     )
